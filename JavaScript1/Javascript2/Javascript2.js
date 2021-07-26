4008b35bfcd7babbe33e234d912885c556a19fbe
renitanathania <renitans16@gmail.com>
2021-07-26 08:40:59 +0700

    JavaScript

---
+++ JavaScript1/Javascript2/Javascript2.js
@@ -0,0 +1,21 @@
+// Nomor 1
+let divisi ='HR';
+    console.log('divisi');
+if (divisi === 'CEO') {
+    console.log('memimpin perusahaan');
+} else if (divisi === 'HR'); {
+    console.log('melakukan rekrutmen untuk calon pegawai baru'); 
+} else if (divisi === 'CTO'); {
+    console.log(ditektur IT);
+} else if (divisi === 'PROGRAMMER'); {
+    console.log(mendesain program);
+} else {
+    console.log(divisi tidak terdaftar)
+}
+
+// Nomor 2
+let nilai a = 5;
+let nilai b = 4;
+ if (a < b){
+     console.log(a => b)
+ }
